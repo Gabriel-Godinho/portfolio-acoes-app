939bd12b64a52ca253b122d89ce5eef1ac59307c
Gabriel Godinho <gabriel234417@gmail.com>
2024-12-03 20:59:15 -0300

    feat: Remoção de ProtectedRoutes

--- src/routes/ProtectedRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Routering from "./routes";
-import { authenticatedUser } from "../services/authentication";
-
-const ProtectedRoute = ({ children }) => {
-  return authenticatedUser() ? children : <Routering />;
-};
-
-export default ProtectedRoute;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import Login from "../pages/login/index";
 import Register from "../pages/register";
 import Home from "../pages/home";
-import ProtectedRoute from "./ProtectedRoutes";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Routering = () => {
@@ -11,14 +10,7 @@ const Routering = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/home"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
